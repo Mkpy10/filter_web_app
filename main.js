cf28eef39f078832a43a3e1c5eafa15186b1d4e9
Mrinank Pandey <79464354+Mkpy10@users.noreply.github.com>
2022-01-26 22:09:32 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function preload(){
+
+}
+
+function setup(){
+    Canvas = createCanvas(300 , 300);
+    Canvas.center();
+}
+
+function draw(){
+
+}
+
+function SnapShot(){
+    save('YouAreAClown.png')
+}
