77b502bb167ac007f99be2564bcdefb495a9f7c5
Mrinank Pandey <79464354+Mkpy10@users.noreply.github.com>
2022-01-27 21:49:06 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -5,12 +5,30 @@ function preload(){
 function setup(){
     Canvas = createCanvas(300 , 300);
     Canvas.center();
+    Webcam = createCapture(VIDEO);
+    Webcam.size(400,300);
+    Webcam.hide()
 }
 
 function draw(){
+    image(Webcam, 0, 0, 400, 300);
+    PoseNet = ml5.poseNet(Webcam , modelLoaded);
+    PoseNet.on('pose', Posing);
 
 }
 
 function SnapShot(){
     save('YouAreAClown.png')
+}
+
+function modelLoaded(){
+    console.log("poseNet is initialised");
+}
+
+function Posing(results){
+    if (results.length > 0) {
+        console.log(results);
+        console.log("Nose X =",+results[0].pose.nose.x);
+        console.log("Nose Y =",+results[0].pose.nose.y);
+    }
 }
